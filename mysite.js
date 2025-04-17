5fa8a5e7ee12f4652f42477c18f96192f97cf53c
PetraDora <p3t4ad0ra@gmail.com>
2025-04-17 14:26:38 +0300

    Add files via upload

---
+++ mysite.js
@@ -0,0 +1,14 @@
+function openPlacestoVisit(){
+    window.location.href="placetovisit.html"
+}
+
+function open5ThingsToDo(){
+    window.location.href="5things.html"
+}
+
+function openStMark(){
+    window.location.href="stmark.html"
+}
+function openHome(){
+    window.location.href="mysite.html"
+}
